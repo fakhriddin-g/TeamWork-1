7ea34854b62bde8749f8737c0be0dc97d081f8c4
Amal <amalabdurahmonov427@gmail.com>
2023-04-27 12:20:37 +0300

    patterns

--- script.js
+++ script.js
@@ -7,10 +7,54 @@ let texterror = document.querySelector('#error')
 let textsuccess = document.querySelector('#success')
 let imgError = document.querySelectorAll('.img-error')
 
+
+
+let inps = form.querySelectorAll('input')
+inps.forEach((inp) => {
+    inp.onkeyup = () => {
+        let key = inp.name
+        patterns[key]
+        validate(patterns[key], inp)
+    }
+})
+
+let patterns = {
+    name: /^[a-z а-я ,.'-]+$/i,
+    surname: /^[a-z а-я ,.'-]+$/i,
+    email:  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/i,
+    number: /^998[012345789][0-9]{8}$/,
+    mumName: /^[a-z а-я ,.'-]+$/i,
+    papName: /^[a-z а-я ,.'-]+$/i,
+    age: /^\S[0-9]{0,2}$/,
+    aboutYou: /^[a-z а-я ,.'-]+$/i,
+    whatIsJS: /^[a-z а-я ,.'-]+$/i,
+    whatIsHTML: /^[a-z а-я ,.'-]+$/i,
+    whatIsCSS: /^[a-z а-я ,.'-]+$/i,
+    yourCar: /^[a-z а-я ,.'-]+$/,
+}
+
+inps.forEach(inp => {
+    inp.onkeyup = () => {
+      let value = inp.value
+      if(!patterns[inp.name].test(value)) {
+        value = value.replace(new RegExp(`[^${patterns[inp.name].source}]`, 'g'), 'i')
+      }
+      inp.value = value
+      if(patterns[inp.name].test(inp.value)) {
+        inp.style.border = '2px solid green'
+      } else {
+        inp.style.border = '2px solid red'
+      }
+    }
+  })
+
+
 form.onsubmit = (event) => {
     event.preventDefault();
     let errors = ''
     let errorCount = 0
+
+
 
     inputs.forEach((inp) => {
         inp.style.border = '2px solid #543FD3'
@@ -34,6 +78,9 @@ form.onsubmit = (event) => {
     })
     })
     }) 
+
+
+
 
     inpNeed.forEach(inp => {
         if(inp.value.length == 0) {
@@ -64,3 +111,20 @@ form.onsubmit = (event) => {
         
     }
 
+
+//   let searchInp = document.querySelector('#search')
+// let pSearch = document.querySelector('.lor-p')
+
+// searchInp.onkeyup = () => {
+//   let re = new RegExp(searchInp.value, 'ig')
+
+//   console.log(re);
+
+//   let text = pSearch.innerHTML
+
+//   if(searchInp.value.length) {
+//     let result = text.replace(re, `<b>${searchInp.value}</b>`)
+
+//     pSearch.innerHTML = result
+//   }
+// }
